c5bf33c31fadd3a625713192d7a2bf8f27b2bd20
nreactor <36671402+eclairAB@users.noreply.github.com>
2022-03-19 18:08:06 +0800

    skekert (#18)

---
+++ app/mixins/connectycube.js
@@ -0,0 +1,15 @@
+import e from '~/environment.js'
+// import ConnectyCube from 'nativescript-connectycube'
+// const cubeClient = require("connectycube");
+
+/*const credentials = {
+    appId: `${e.CONNECTYCUBE_APPID}`,
+    authKey: `${e.CONNECTYCUBE_AUTH_KEY}`,
+    authSecret: `${e.CONNECTYCUBE_AUTH_SECRET}`,
+};
+
+const config = {
+    debug: { mode: 1 }
+};
+
+ConnectyCube.init(credentials, config);*/
--- app/urls.js
+++
@@ -1,5 +0,0 @@
-let urls = {
-    API_URL: 'http://202.137.120.41:8089/api'
-}
-
-export default urls
